1641ddab922331287e44145a21d9b46828c4c7b1
Gonzalo Diez <pinkiibiza@gmail.com>
2015-06-29 18:12:07 +0200

    Inifinite Parallax Scrolling
    All the backgrounds and foregrounds that needs to move on loop is done.

---
+++ Penguin Rush/Assets/Scripts/AutoMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMovement : MonoBehaviour {
+
+	public float speed = 0;
+	public int size = 19;
+
+	private float pos = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		float movement = -speed*Time.deltaTime;
+		pos += movement;
+		if (transform.position.x < -size) {
+			pos += 2*size;
+			transform.Translate(new Vector3(2*size,0,0));
+		}
+		transform.Translate(new Vector3(movement,0,0));
+	}
+}
