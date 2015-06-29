5afba19159a76917e73a04a93e46cad25c63321c
Gonzalo Diez <pinkiibiza@gmail.com>
2015-06-29 13:14:44 +0200

    Player movement
    First touch with sprite movements and geting input :dancer:

---
+++ Penguin Rush/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(50,50);
+
+	private Vector2 movement;
+
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		movement = new Vector2( speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate() {
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
