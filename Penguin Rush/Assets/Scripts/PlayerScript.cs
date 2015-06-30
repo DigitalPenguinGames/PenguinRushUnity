b1a35ca54770cf4956205b50e1e3b804dbc562e6
Gonzalo Diez <pinkiibiza@gmail.com>
2015-06-30 17:47:43 +0200

    Working on the player script
    Continuing with the player script a little and installed the WakaTime
    plugin for Unity

--- Penguin Rush/Assets/Scripts/PlayerScript.cs
+++ Penguin Rush/Assets/Scripts/PlayerScript.cs
@@ -2,11 +2,10 @@
 
 public class PlayerScript : MonoBehaviour {
 
-	public Vector2 speed = new Vector2(50,0);
-
-	public float accelerationJump = 100;
-	public float gravity = 9.8f;
-
+	public Vector2 speed = new Vector2(50,20);
+	
+	public float gravity = 2f;
+	public float center = -0.15f;
 
 	private Vector2 movement = new Vector2(0,0);
 	private bool jumping = false;
@@ -14,7 +13,7 @@ public class PlayerScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.y < 1 && transform.position.y > -1) {
+		if (transform.position.y < center + 1 && transform.position.y > center -1) {
 			jumping = false;
 		}
 		else jumping = true;
@@ -22,13 +21,13 @@ public class PlayerScript : MonoBehaviour {
 			int inputY = 0;
 			if (Input.GetAxis("Vertical") > 0) inputY = 1;
 			else if (Input.GetAxis("Vertical") < 0) inputY = -1;
-			movement = new Vector2(0 , accelerationJump * inputY);
+			movement = new Vector2(0 , speed.y * inputY);
 			jumping = (inputY != 0);
 			//directionUp = (inputY == 1);
 			print(inputY);
 		}
 		// Gravity
-		if (transform.position.y > 0) movement.y -= gravity - Time.deltaTime;
+		if (transform.position.y > center) movement.y -= gravity - Time.deltaTime;
 		else movement.y += gravity - Time.deltaTime;
 	}
 
