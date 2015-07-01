255e20f7690965e7a68656911929c30da26ed4ba
Gonzalo Diez <pinkiibiza@gmail.com>
2015-07-01 19:10:30 +0200

    The player's jump is real!
    The script that manage the acceleration and speed of the player while
    jumping is fixed.

--- Penguin Rush/Assets/Scripts/PlayerScript.cs
+++ Penguin Rush/Assets/Scripts/PlayerScript.cs
@@ -2,10 +2,14 @@
 
 public class PlayerScript : MonoBehaviour {
 
-	public Vector2 speed = new Vector2(50,20);
+	public float jumptime = 1;
+	public float dist = 4;
+
+	//public Vector2 speed = new Vector2(50,0);
 	
-	public float gravity = 2f;
+	public float gravity = 0f;
 	public float center = -0.15f;
+	public float interval = 0.5f;
 
 	private Vector2 movement = new Vector2(0,0);
 	private bool jumping = false;
@@ -13,7 +17,7 @@ public class PlayerScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.y < center + 1 && transform.position.y > center -1) {
+		if (transform.position.y < center + interval && transform.position.y > center - interval) {
 			jumping = false;
 		}
 		else jumping = true;
@@ -21,17 +25,27 @@ public class PlayerScript : MonoBehaviour {
 			int inputY = 0;
 			if (Input.GetAxis("Vertical") > 0) inputY = 1;
 			else if (Input.GetAxis("Vertical") < 0) inputY = -1;
-			movement = new Vector2(0 , speed.y * inputY);
-			jumping = (inputY != 0);
-			//directionUp = (inputY == 1);
+			if (inputY != 0) {
+				float factor = 2f/(jumptime * jumptime);
+				gravity = - factor * ((dist + center) - transform.position.y);
+				movement.y = - gravity * jumptime;
+				movement.y *= inputY;
+				jumping = true;
+			}
+			else jumping = false;
 			print(inputY);
 		}
 		// Gravity
-		if (transform.position.y > center) movement.y -= gravity - Time.deltaTime;
-		else movement.y += gravity - Time.deltaTime;
+
 	}
 
 	void FixedUpdate() {
-		GetComponent<Rigidbody2D>().velocity = movement;
+		if (transform.position.y > center) movement.y += gravity*Time.deltaTime;
+		else movement.y -= gravity*Time.deltaTime;
+		if (transform.position.y > center && transform.position.y + movement.y*Time.deltaTime < center
+		    || transform.position.y < center && transform.position.y + movement.y*Time.deltaTime > center) 
+			movement.y *= 0.5f;
+		GetComponent<Rigidbody2D>().velocity = movement;	
+		print("gravity " + gravity  + " movement: " + movement.y);
 	}
 }
