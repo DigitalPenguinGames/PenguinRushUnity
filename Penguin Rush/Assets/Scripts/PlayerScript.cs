d31e6e14a15a2e97882af97127bd44d1e9ef2ecb
Gonzalo Diez <pinkiibiza@gmail.com>
2015-07-02 09:19:04 +0200

    PlayerScript finished for now :+1:
    The PlayerScript is moving the player like i want :D next move: Spawn
    the obstacles and make the colisions with the obstacles.

--- Penguin Rush/Assets/Scripts/PlayerScript.cs
+++ Penguin Rush/Assets/Scripts/PlayerScript.cs
@@ -2,50 +2,56 @@
 
 public class PlayerScript : MonoBehaviour {
 
-	public float jumptime = 1;
-	public float dist = 4;
-
-	//public Vector2 speed = new Vector2(50,0);
-	
-	public float gravity = 0f;
-	public float center = -0.15f;
-	public float interval = 0.5f;
+	public float jumptime = 0.4f;
+	public float dist = 4.5f;
+	public Vector2 center = new Vector2(0,-0.15f);
 
 	private Vector2 movement = new Vector2(0,0);
-	private bool jumping = false;
-//	private bool directionUp;
+	private float gravity = 0f;
+
+	private enum dir {
+		up, down, none
+	};
+	private dir lastJump = dir.none;
+
+	void Awake() {
+		transform.position = center;
+	}
 
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.y < center + interval && transform.position.y > center - interval) {
-			jumping = false;
-		}
-		else jumping = true;
-		if (!jumping) {
+		if (lastJump == dir.none) {
 			int inputY = 0;
-			if (Input.GetAxis("Vertical") > 0) inputY = 1;
-			else if (Input.GetAxis("Vertical") < 0) inputY = -1;
+			if (Input.GetAxis("Vertical") > 0) {
+				inputY = 1;
+				lastJump = dir.up;
+			}
+			else if (Input.GetAxis("Vertical") < 0) {
+				inputY = -1;
+				lastJump = dir.down;
+			}
 			if (inputY != 0) {
 				float factor = 2f/(jumptime * jumptime);
-				gravity = - factor * ((dist + center) - transform.position.y);
+				gravity = - factor * ((dist + center.y) - transform.position.y);
 				movement.y = - gravity * jumptime;
 				movement.y *= inputY;
-				jumping = true;
 			}
-			else jumping = false;
-			print(inputY);
+			//print(inputY);
 		}
-		// Gravity
 
 	}
 
 	void FixedUpdate() {
-		if (transform.position.y > center) movement.y += gravity*Time.deltaTime;
+		// Gravity
+		if (transform.position.y > center.y) movement.y += gravity*Time.deltaTime;
 		else movement.y -= gravity*Time.deltaTime;
-		if (transform.position.y > center && transform.position.y + movement.y*Time.deltaTime < center
-		    || transform.position.y < center && transform.position.y + movement.y*Time.deltaTime > center) 
+		if (transform.position.y > center.y && transform.position.y + movement.y*Time.deltaTime < center.y
+		    || transform.position.y < center.y && transform.position.y + movement.y*Time.deltaTime > center.y) {
 			movement.y *= 0.5f;
+			lastJump = dir.none;
+		}
 		GetComponent<Rigidbody2D>().velocity = movement;	
-		print("gravity " + gravity  + " movement: " + movement.y);
+
+		//print("gravity " + gravity  + " movement: " + movement.y);
 	}
 }
