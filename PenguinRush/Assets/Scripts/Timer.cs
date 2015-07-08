247fc2ed90e08a3390f77729e553860df870bd82
Gonzalo Diez <pinkiibiza@gmail.com>
2015-07-08 19:13:18 +0200

    High Scores

--- PenguinRush/Assets/Scripts/Timer.cs
+++ PenguinRush/Assets/Scripts/Timer.cs
@@ -6,13 +6,17 @@ public class Timer : MonoBehaviour {
 
 	public float factor = 1;
 	public Text score;
+	public Text highscoreT;
 
 	private float time = 0;
 	private bool run = true;
+	private float highscore = 0;
 
 	// Use this for initialization
 	void Start () {
 		score.text = "Score : 0";
+		highscore = PlayerPrefs.GetFloat("HighScore");
+		highscoreT.text = "High Score : " + highscore.ToString("F0");
 	}
 	
 	// Update is called once per frame
@@ -20,10 +24,18 @@ public class Timer : MonoBehaviour {
 		if (run) {
 			time += Time.deltaTime;
 			score.text = "Score : " + (time * factor).ToString("F0");
+			if (time*factor > highscore) {
+				highscore = time*factor;
+				highscoreT.text = "Highscore : " + highscore.ToString("F0");
+			}
 		}
 	}
 
 	public void stop() {
 		run = false;
+	}
+
+	public void saveScore() {
+		PlayerPrefs.SetFloat("HighScore",highscore);
 	}
 }
